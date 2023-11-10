0685a20940a4af3842bea1d7f390048e90c0eb59
Santu <j6458221@yahoo.com.tw>
2023-11-10 23:59:20 +0800

    新增header(手機板)，調整banner(手機板)

--- layout/header.js
+++ layout/header.js
@@ -1,14 +1,23 @@
 // 點擊餐點介紹時收合
 const selectMenu = document.querySelector(".selectMenu");
-const dropdown = document.querySelector(".dropdown");
+const dropdownMenu = document.querySelector(".dropdownMenu");
+const hamberMenu = document.querySelector(".hamberMenu");
+const dropdownNavbar = document.querySelector(".dropdownNavbar");
 // console.log(selectMenu);
 
 selectMenu.addEventListener("click", function (e) {
   // console.log(e.target.nodeName);
   e.preventDefault();
   if (e.target.nodeName == "A" || e.target.nodeName == "SPAN") {
-    dropdown.classList.toggle("active");
+    dropdownMenu.classList.toggle("active");
     // 展開時餐點介紹為focus狀態
     selectMenu.classList.toggle("focused");
+  }
+});
+
+hamberMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.nodeName == "A" || e.target.nodeName == "SPAN") {
+    dropdownNavbar.classList.toggle("active");
   }
 });

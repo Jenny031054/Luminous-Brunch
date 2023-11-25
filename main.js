a45b2666369a0d02b4cffc85b6c36cbc22fc1a8e
林炘秀 <jenny031054@gmail.com>
2023-11-25 20:51:18 +0800

    新增菜單分頁+菜單內頁選單ejs

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './layout/header';
 import './pages/5-1_booking.js';
+import './pages/4_menu.js'
 
 // console.log('Hello world!!!');
 // console.log('Hello world*2!!!');

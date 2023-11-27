23478bb8bbcbaf919e7bf3d9616d09cd4f11d18d
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2023-11-27 11:40:19 +0800

    調整axios CDN

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './layout/header';
 import './pages/5-1_booking.js';
 import './pages/4_menu.js';
-import './pages/5-1-1_booking.js';
 // console.log('Hello world!!!');
 // console.log('Hello world*2!!!');
 // console.log('成功加入');

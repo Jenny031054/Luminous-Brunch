3d5d9e12ea0e728d3554a5fca893b94d1b9d3a25
林炘秀 <jenny031054@gmail.com>
2023-11-27 23:44:54 +0800

    菜單API修改

--- pages/4_menu.js
+++ pages/4_menu.js
@@ -171,7 +171,7 @@ let menuData =
         },
         {   
             id:6,
-            title:"漢堡排佐嫰蛋早餐盤",
+            title:"漢堡排佐嫩蛋早餐盤",
             price:250,
             imgUrl:"https://images.unsplash.com/photo-1525184782196-8e2ded604bf7?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },

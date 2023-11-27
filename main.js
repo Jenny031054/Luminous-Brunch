f774da5f5247462223b4c67215b43dd2ebed16f2
SHUO <iwilliam147258@gmail.com>
2023-11-27 13:14:59 +0800

    新增login

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import './layout/header';
 import './pages/5-1_booking.js';
 import './pages/4_menu.js';
 import './pages/_register.js';
+import './pages/_login.js';
 // console.log('Hello world!!!');
 // console.log('Hello world*2!!!');
 // console.log('成功加入');
---
+++ pages/_login.js
@@ -0,0 +1,29 @@
+const inputEmail = document.querySelector('#inputEmail');
+const inputPassword = document.querySelector('#inputPassword');
+const loginBtn = document.querySelector('#login');
+
+let token = "";
+loginBtn.addEventListener("click",function(){
+    if(inputEmail.value.trim() == "" || inputPassword.value.trim() == ""){
+        alert("請填寫帳號/密碼")
+        return;
+    };
+    let obj = {};        
+        obj.email = inputEmail.value;
+        obj.password = inputPassword.value;
+        login(obj);
+})
+
+function login(obj){
+    axios.post('https://json-server-auth-q2r9.onrender.com/users/login', obj)
+    .then(function(res){
+        if (res.status == 200) {
+            window.location.assign("index.html");
+          }else{
+            alert("Login failed. Please check your credentials.")
+          }
+    })
+    .catch(function(error){
+        console.log(error.response);
+    })
+}

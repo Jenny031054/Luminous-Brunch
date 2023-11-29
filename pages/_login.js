3e6bdc6385174b728acfb7e44df2fe9c8f91b9bb
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2023-11-29 19:31:11 +0800

    調整login監聽範圍

--- pages/_login.js
+++ pages/_login.js
@@ -2,29 +2,32 @@ const inputEmail = document.querySelector('#inputEmail');
 const inputPassword = document.querySelector('#inputPassword');
 // const loginBtn = document.querySelector('#login');
 const form = document.querySelector('form');
-let token = "";
-form.addEventListener("submit", function(e){
+let token = '';
+if (window.location.href.includes('_login.html')) {
+  form.addEventListener('submit', function (e) {
     e.preventDefault();
-    if(inputEmail.value.trim() == "" || inputPassword.value.trim() == ""){
-        alert("請填寫帳號/密碼")
-        return;
-    };
-    let obj = {};        
-        obj.email = inputEmail.value;
-        obj.password = inputPassword.value;
-        login(obj);
-})
+    if (inputEmail.value.trim() == '' || inputPassword.value.trim() == '') {
+      alert('請填寫帳號/密碼');
+      return;
+    }
+    let obj = {};
+    obj.email = inputEmail.value;
+    obj.password = inputPassword.value;
+    login(obj);
+  });
 
-function login(obj){
-    axios.post('https://json-server-auth-q2r9.onrender.com/login', obj)
-    .then(function(res){
+  function login(obj) {
+    axios
+      .post('https://json-server-auth-q2r9.onrender.com/login', obj)
+      .then(function (res) {
         if (res.status == 200) {
-            window.location.assign("index.html");
-          }else{
-            alert("Login failed. Please check your credentials.")
-          }
-    })
-    .catch(function(error){
+          window.location.assign('index.html');
+        } else {
+          alert('Login failed. Please check your credentials.');
+        }
+      })
+      .catch(function (error) {
         console.log(error.response);
-    })
+      });
+  }
 }

59f389c81354aa77f028e602a66d39e75d6cf065
desmend2234 <143927374+desmend2234@users.noreply.github.com>
2023-12-01 17:32:05 +0800

    訂位功能完成

--- pages/5-1-1_booking.js
+++ pages/5-1-1_booking.js
@@ -5,8 +5,6 @@ import axios from 'axios';
 if (window.location.href.includes('5-1-1_booking.html')) {
   const storedFormData = JSON.parse(localStorage.getItem('formData'));
   const formConfirmContent = document.querySelector('.form-content-confirm');
-  const cancelBtn = document.querySelector('#cancel-btn');
-  const deleteBtn = document.querySelector('#delete-btn');
   let htmlFinal = '';
 
   bookingContents(storedFormData);
@@ -17,20 +15,24 @@ if (window.location.href.includes('5-1-1_booking.html')) {
       .get(`${postUrl}?name=${storedFormData.name}`)
       .then(() => {})
       .catch((error) => console.log(error));
-    htmlFinal = `<div class="booking-Contents"><p>您已完成訂位，訂位資訊如下：</p>
-            <p>用餐人數：${storedFormData.peopleNumber}</p>
-            <p>日期：${storedFormData.date}</p>
-            <button type="button" class="btn btn-primary" id="cancel-btn">
-                取消訂位
-              </button>
-              <a class="btn btn-primary" href="index.html" role="button"
-                >回首頁</a
-              ></div>`;
+    htmlFinal = `
+    <div class="booking-Contents">
+        <p>您已完成訂位，訂位資訊如下：</p>
+        <p>用餐人數：${storedFormData.peopleNumber}</p>
+        <p>日期：${storedFormData.date}</p>
+    </div>
+    <div class='form-btn-group'>
+        <button type="button " class="btn btn-primary form-btn" id="cancel-btn">取消訂位</button>
+        <a class="btn btn-primary form-btn" href="index.html" role="button">回首頁</a>
+      
+    </div>`;
     formConfirmContent.innerHTML = htmlFinal;
   }
   cancelBooking(storedFormData);
 
   function cancelBooking(storedFormData) {
+    let cancelBtn = document.querySelector('#cancel-btn');
+
     cancelBtn.addEventListener('click', (e) => {
       e.preventDefault();
       htmlFinal = `<div class="booking-Contents"><p>訂位資訊如下：</p>
@@ -38,9 +40,9 @@ if (window.location.href.includes('5-1-1_booking.html')) {
   <p>日期：${storedFormData.date}</p>
   <p>是否確認取消訂位？</p></div>
   <div class="d-flex justify-content-evenly mt-5" id="">
-  <button type="button" class="btn btn-primary" id="">
-                否</button
-              ><button type="button" class="btn btn-primary" id="delete-btn"
+  <a class="btn btn-primary form-btn" href="5-1-1_booking.html" role="button" id="">
+  否</a
+        ><button type="button " class="btn btn-primary form-btn" id="delete-btn"
                 >取消訂位</button
               ></div>`;
       formConfirmContent.innerHTML = htmlFinal;
@@ -49,6 +51,8 @@ if (window.location.href.includes('5-1-1_booking.html')) {
   }
 
   function deleteBooking(storedFormData) {
+    let deleteBtn = document.querySelector('#delete-btn');
+
     deleteBtn.addEventListener('click', (e) => {
       e.preventDefault();
       console.log(storedFormData);
@@ -61,9 +65,9 @@ if (window.location.href.includes('5-1-1_booking.html')) {
         <p>日期：${storedFormData.date}</p>
         <p>時間：${storedFormData.time}</p></div>
         <div class="d-flex justify-content-evenly mt-5" id="">
-        <a class="btn btn-primary" href="index.html" role="button" id="">
+        <a class="btn btn-primary form-btn" href="index.html" role="button" id="">
                       回首頁</a
-                    ><a class="btn btn-primary" href="5-1_booking.html" role="button" id=""
+                    ><a class="btn btn-primary form-btn" href="5-1_booking.html" role="button" id=""
                       >重新訂位</a
                     ></div>`;
       formConfirmContent.innerHTML = htmlFinal;
--- pages/5-1_booking.js
+++ pages/5-1_booking.js
@@ -4,10 +4,9 @@ import { postUrl } from './config';
 
 const form = document.querySelector('form');
 const steps = document.querySelectorAll('.step');
-const submitBtn = document.querySelector('#submitBtn');
 const fields = ['peopleNumber', 'date', 'time', 'name', 'phoneNumber', 'email'];
 let formData;
-
+let html;
 //將表單資料統一儲存
 export const formDataManager = (function () {
   return {
@@ -24,13 +23,13 @@ if (window.location.href.includes('5-1_booking.html')) {
   restoreBooking();
   //儲存訂位資料
   function restoreBooking() {
+    form.addEventListener('submit', clickHandler);
     function clickHandler(e) {
       e.preventDefault();
       if (!e) return;
       const dataArr = [...new FormData(form)];
       formDataManager.setFormData(Object.fromEntries(dataArr));
       formData = formDataManager.getFormData();
-
       axios
         .post(postUrl, formData)
         .then((res) => {
@@ -41,152 +40,31 @@ if (window.location.href.includes('5-1_booking.html')) {
       steps[0].classList.remove('active');
       steps[1].classList.add('active');
     }
-    submitBtn.addEventListener('click', clickHandler);
   }
 
   //確認訂位資料
   function renderConfirm(formData) {
-    console.log(formData);
+    //撈取訂位資料
     localStorage.setItem('formData', JSON.stringify(formData));
-
     const btnControl = document.querySelector('#btn-control');
     //載入填寫內容
     fields.map((item) => {
       const inputElement = document.querySelector(`#${item}`);
       inputElement.value = formData[item];
+      //資料不可再編輯
       inputElement.disabled = true;
     });
-    const buttons = [
-      {
-        type: 'button',
-        text: '重新填寫',
-        id: 'rewrite',
-        onClick: handleRewrite,
-      },
-      {
-        type: 'submit',
-        text: '確認',
-        id: 'submitValue',
-        onClick: handleConfirm,
-      },
-    ];
+    html = `<a class="btn btn-primary form-btn form-btn" href="5-1_booking.html" role="button"
+    >重新填寫</a
+  ><button type="submit" class="btn btn-primary form-btn" id="submitValue">
+  確認
+  </button>`;
+    btnControl.innerHTML = html;
 
-    btnControl.innerHTML = '';
-    //將buttons資料用迴圈賦予值
-    buttons.forEach((buttonInfo) => {
-      const button = document.createElement('button');
-      button.type = buttonInfo.type;
-      button.className = 'btn btn-primary';
-      button.id = buttonInfo.id;
-      button.innerText = buttonInfo.text;
-
-      button.addEventListener('click', buttonInfo.onClick);
-      btnControl.appendChild(button);
-    });
-
-    handleRewrite();
     handleConfirm(formData);
   }
-  //重新填寫按鈕
-  function handleRewrite() {
-    const rewriteBtn = document.querySelector('#rewrite');
-    rewriteBtn.addEventListener('click', (e) => {
-      let html = `<div class="step-indicator">
-    <div class="step active" id="step1"><span>填寫預訂</span></div>
-    <div class="line"></div>
-    <div class="step" id="step2"><span>資料確認</span></div>
-    <div class="line"></div>
-    <div class="step" id="step3"><span>訂位完成</span></div>
-  </div>
-  <div id="formContent">
-    <div class="mb-3">
-      <label for="peopleNumber" class="form-label">用餐人數</label>
-      <select
-        class="form-select"
-        aria-label="Default select example"
-        id="peopleNumber"
-        name="peopleNumber"
-        required
-      >
-        <option value="1" selected>1位</option>
-        <option value="2">2位</option>
-        <option value="3">3位</option>
-        <option value="4">4位</option>
-      </select>
-
-      <div class="form-text">
-        <label for="date" class="form-label">用餐日期</label>
-        <label for="time" class="form-label">時間</label>
-      </div>
-      <div class="d-flex justify-content-between">
-        <input
-          type="date"
-          class="form-control"
-          id="date"
-          name="date"
-          required
-        /><input
-          type="time"
-          id="time"
-          name="time"
-          min="09:00"
-          max="18:00"
-          required
-          class="form-control"
-        />
-      </div>
-    </div>
-    <div class="mb-3">
-      <label for="name" class="form-label">姓名</label>
-      <input
-        type="text"
-        class="form-control"
-        id="name"
-        placeholder="範例:陳曉明"
-        style="width: 100%"
-        name="name"
-        required
-      />
-    </div>
-    <div class="mb-3">
-      <label for="phoneNumber" class="form-label">電話</label>
-      <input
-        type="tel"
-        class="form-control"
-        id="phoneNumber"
-        placeholder="範例:0939123456"
-        style="width: 100%"
-        name="phoneNumber"
-        required
-      />
-    </div>
-    <div class="mb-3">
-      <label for="email" class="form-label">信箱</label>
-      <input
-        type="email"
-        class="form-control"
-        id="email"
-        placeholder="範例:abc@gmail.com"
-        style="width: 100%"
-        name="email"
-        required
-      />
-    </div>
-    <div class="d-flex justify-content-evenly mt-5" id="btn-control">
-      <button
-        type="click"
-        class="btn btn-primary act-btn"
-        id="submitBtn"
-      >
-        立即訂位
-      </button>
-    </div>
-  </div>`;
-      form.innerHTML = html;
-    });
-  }
   //確認訂位
-  function handleConfirm(formData) {
+  function handleConfirm() {
     form.addEventListener('submit', function clickFunction(e) {
       e.preventDefault();
       console.log('完成跳轉');

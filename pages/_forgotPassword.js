3471ad6734b8eab7eaf8cc52bc6f882cbd454749
林炘秀 <jenny031054@gmail.com>
2023-12-10 22:36:55 +0800

    新增忘記密碼修改密碼功能

--- pages/_forgotPassword.js
+++ pages/_forgotPassword.js
@@ -1,46 +1,59 @@
 ////功能需求：輸入正確的信箱、驗證碼(先設定有輸入數字就好)後跳轉到輸入新密碼畫面，如果兩格密碼相符的話就發送put請求，更改該帳號的密碼，畫面則跳轉到"密碼更改完成"
 if (window.location.href.includes('password.html')) {
   const submitValidateEmail = document.querySelector('.submitValidateEmail');
-  const passwordNewEmail = document.querySelector(
-    'input[name="passwordNewEmail"]'
-  );
+  const passwordNewEmail = document.querySelector('input[name="passwordNewEmail"]');
   const submitValidateCode = document.querySelector('.submitValidateCode');
   const validateCode = document.querySelector('input[name="validateCode"]');
   const loginBody = document.querySelector('.login-body');
   const inputs = document.querySelectorAll('input');
-  console.log(inputs);
-
-  // 等等把點擊裡面會處發的程式包在一個函示裡面
+  // 等等把點擊裡面會將使用者的信箱帶入找ID的函式參數中，
+  // 宣告放置user資訊的物件
+  let usersData = {};
+  let userId ;
   submitValidateEmail.addEventListener('click', (e) => {
     submitValidateEmail.textContent = '已發送';
-
-    let newEmail = passwordNewEmail.value;
-    console.log(newEmail);
+    let userEmail = passwordNewEmail.value;
+    
+    axios.get('https://json-server-auth-q2r9.onrender.com/users')
+      .then(function(res){
+        console.log(res.data);
+        usersData = res.data;
+        searchUserId(userEmail)
+      })
+      .catch(function(err){
+        console.log(err)
+      });
+   
   });
-
+  // 尋找相同的email，抓出使用者id
+  function searchUserId(userEmail){
+    usersData.forEach((item)=>{
+      if(userEmail == item.email){
+        userId = item.id;
+      }else{
+        alert("此信箱尚無會員資料，請先註冊帳號");
+      }
+    })
+  } 
   // 點擊送出功能
   submitValidateCode.addEventListener('click', (e) => {
-    showErrorMessage(inputs);
+    showErrorMessage(inputs,userId);
   });
 
   // 監聽整個loginBody
   loginBody.addEventListener('click', (e) => {
     if (e.target.getAttribute('id') == 'confrimPasswordBtn') {
       const resetPasswordInput = document.querySelectorAll('input');
-      console.log(resetPasswordInput);
       showErrorMessage(resetPasswordInput);
-      console.log(e.target.getAttribute('id'));
     }
   });
 
   const errorMessage = document.querySelectorAll('.errorMessage');
-  //驗證input狀況
+  //驗證第一頁忘記密碼 input的狀況
   function showErrorMessage(inputs) {
     let passInputNum = 0;
-    console.log(inputs);
     inputs.forEach((i, index) => {
       if (i.value == '') {
-        console.log(errorMessage[index]);
         errorMessage[index].classList.add('text-warning');
         errorMessage[index].textContent = '此欄不得為空';
       } else {
@@ -49,27 +62,25 @@ if (window.location.href.includes('password.html')) {
       }
     });
     if (passInputNum >= 2) {
-      setNewPassword();
+      setNewPassword(userId);
     }
   }
 
   //跳轉填寫新密碼頁面
-  function setNewPassword() {
+  function setNewPassword(userId) {
     let str = `
   <h1 class="h3 mb-3 font-weight-normal text-center text-light">重置密碼</h1>
     <p class="text-light">密碼請輸入6-12碼，英文與數字混和</p>
       <label for="inputNewpassword" class="sr-only text-light">輸入新密碼</label>
         <input
           type="password"
-          name="passwordNewEmail"
+          name="inputNewpassword"
           id="inputNewpassword"
           class="form-control w-100 mb-3"
           placeholder="重置新密碼"
           required
           autofocus
         />
-        <!-- 放錯誤訊息的區塊 -->
-        <p class="errorMessage"></p>
         <label for="confrimPassword" class="sr-only text-light">再次輸入新密碼</label>
         <input
           type="password"
@@ -81,12 +92,12 @@ if (window.location.href.includes('password.html')) {
           autofocus
         />
         <!-- 放錯誤訊息的區塊 -->
-        <p class="errorMessage"></p>
+        <p class="newErrorMessage"></p>
         <div class="text-center mt-4">
           <button
             class="btn btn-lg btn-blue btn-block w-100"
             type="submit"
-            id="confrimPasswordBtn"
+            id="confrimNewPasswordBtn"
           >
             確認
           </button>
@@ -94,17 +105,47 @@ if (window.location.href.includes('password.html')) {
       
   `;
   loginBody.innerHTML = str;
-  const confrimPasswordBtn = document.querySelector("#confrimPasswordBtn");
-  confrimPasswordBtn.addEventListener('click',(e)=>{
-    finishPassword()
+  // 監聽這邊的修改密碼按鈕，有驗證成功就發patch請求並跳轉頁面
+  const inputNewpassword = document.querySelector("#inputNewpassword");
+  const confrimPassword = document.querySelector("#confrimPassword");
+  const confrimNewPasswordBtn = document.querySelector("#confrimNewPasswordBtn");
+  confrimNewPasswordBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const newErrorMessage = confrimPassword.nextElementSibling;
+    if(inputNewpassword.value == confrimPassword.value && inputNewpassword.value !== "" &&confrimPassword.value !== "" && inputNewpassword.value.length >= 6 && confrimPassword.value.length >= 6){
+      patchNewPassword(inputNewpassword.value,userId)
+    }
+    else if(inputNewpassword.value !== confrimPassword.value && inputNewpassword.value !== "" &&confrimPassword.value !== ""){
+      newErrorMessage.classList.add("d-block");
+      newErrorMessage.classList.add("text-warning");
+      newErrorMessage.textContent= '新密碼與再次輸入密碼不一致';
+    }
+    else if(inputNewpassword.value.length < 6 || confrimPassword.value.length <6){
+      newErrorMessage.classList.add("d-block");
+      newErrorMessage.classList.add("text-warning");
+      newErrorMessage.textContent= '密碼請輸入6-12碼，英文與數字混和';
+    };
+    
   })
   
 };
 
-//跳轉 "密碼重製成功"
-function finishPassword(){
+//跳轉 "密碼重置成功"
+function patchNewPassword(newPassword,userId){
   let str = "";
-
+  // 發patch請求
+  axios.patch(`https://json-server-auth-q2r9.onrender.com/users/${userId}`,
+  {
+    password:newPassword
+  }
+  )
+  .then((res)=>{
+    console.log(res)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+  // 跳轉頁面的HTML字串
   str =
   `
   <div class="d-flex justify-content-center mb-2">
